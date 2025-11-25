378ca0ef8136b1b5b909138a0f2a459dd031bbe0
Manjeet-Yadav-777 <my79762975@gmail.com>
2025-11-25 14:52:19 +0530

    Access First and Last element Solved

---
+++ DSA/Array/3 Access First and Last Element/script.js
@@ -0,0 +1,11 @@
+function Access(arr) {
+  let last = arr[arr.length - 1];
+  let first = arr[0];
+
+  console.log("First = ", first);
+  console.log("Last = ", last);
+}
+
+let arr = [3, 4, 5, 3, 6, 8];
+
+Access(arr);
